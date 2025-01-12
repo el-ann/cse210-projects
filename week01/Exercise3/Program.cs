3cf7767bdef81b65f04bc6d1e665243918b259d9
Elizabeth Ann Adjabeng <ewuradwoaann@gmail.com>
2025-01-12 20:12:59 +0000

    exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,41 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        static void Main(string[] args)
+        {
+            bool playAgain = true;
+            while (playAgain)
+            {
+                Random randomNumber = new Random();
+                int magicNumber = randomNumber.Next(1, 101);
+
+                int guess = -1; 
+                int numberOfGuesses = 0; 
+                while (guess != magicNumber)
+                {
+                    Console.WriteLine("What is your guess?");
+                    guess = int.Parse(Console.ReadLine());
+                    numberOfGuesses++;
+
+                    if (guess < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (guess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed it!");
+                        Console.WriteLine($"It took you {numberOfGuesses} guesses.");
+                    }
+                }
+
+                Console.WriteLine("Do you want to play again? (yes/no)");
+                string response = Console.ReadLine().ToLower();
+                playAgain = response == "yes";
+            }
+        }
     }
-}
