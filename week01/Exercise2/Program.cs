3499713bdf893c98059b4b91d75c497969f13097
Elizabeth Ann Adjabeng <ewuradwoaann@gmail.com>
2025-01-12 04:39:05 +0000

    exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,45 +1,73 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Enter your grade percentage:");
-        int gradePercentage = int.Parse(Console.ReadLine());
-        
-        string letter;
+        static void Main(string[] args)
+        {
+            // Prompt the user for their grade percentage
+            Console.WriteLine("Enter your grade percentage:");
+            int gradePercentage = int.Parse(Console.ReadLine());
 
-        if (gradePercentage >= 90)
-        {
-            letter = "A";
-        }
-        else if (gradePercentage >= 80)
-        {
-            letter = "B";
-        }
-        else if (gradePercentage >= 70)
-        {
-            letter = "C";
-        }
-        else if (gradePercentage >= 60)
-        {
-            letter = "D";
-        }
-        else
-        {
-            letter = "F";
-        }
+            string letter;
+            string sign = "";
 
-        Console.WriteLine($"Your letter grade is: {letter}");
+            // Determine the letter grade
+            if (gradePercentage >= 90)
+            {
+                letter = "A";
+            }
+            else if (gradePercentage >= 80)
+            {
+                letter = "B";
+            }
+            else if (gradePercentage >= 70)
+            {
+                letter = "C";
+            }
+            else if (gradePercentage >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
 
-        if (gradePercentage >= 70)
-        {
-            Console.WriteLine("Congratulations! You passed the course");
-        }
-        else
-        {
-            Console.WriteLine("Keep trying! You'll get it next time.");
+            // Determine the sign
+            if (letter != "A" && letter != "F")
+            {
+                int lastDigit = gradePercentage % 10;
+                if (lastDigit >= 7)
+                {
+                    sign = "+";
+                }
+                else if (lastDigit < 3)
+                {
+                    sign = "-";
+                }
+            }
+
+            // Handle exceptional cases
+            if (letter == "A" && sign == "+")
+            {
+                sign = "";
+            }
+            else if (letter == "F")
+            {
+                sign = "";
+            }
+
+            // Print the letter grade with the sign
+            Console.WriteLine($"Your letter grade is: {letter}{sign}");
+
+            // Determine if the user passed the course
+            if (gradePercentage >= 70)
+            {
+                Console.WriteLine("Congratulations! You passed the course.");
+            }
+            else
+            {
+                Console.WriteLine("Keep trying! You'll get it next time.");
+            }
         }
     }
-
-}
