a176dbee9448b2464a034334e8a4dc53153435ec
Elizabeth Ann Adjabeng <ewuradwoaann@gmail.com>
2025-02-06 22:49:52 +0000

    inheritance assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Lucy Ofori", "Creative Arts");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("David Koomson", "Trigonometry", "1.3", "15-21");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("King Joseph", "African History", "The Slave Trade");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
