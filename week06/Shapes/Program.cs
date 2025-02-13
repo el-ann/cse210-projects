75b9788ad166cadcb21a9d5a8ecb43ab99081eff
Elizabeth Ann Adjabeng <ewuradwoaann@gmail.com>
2025-02-13 22:22:00 +0000

    shapes commit

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Purple", 3),
+            new Rectangle("Orange", 2, 5),
+            new Circle("Turqoise", 4)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.Color} shape has an area of {shape.GetArea()}.");
+        }
     }
 }
