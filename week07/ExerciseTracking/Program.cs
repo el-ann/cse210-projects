c99e8166efc7f139e11997727deff7f734846df5
Elizabeth Ann Adjabeng <ewuradwoaann@gmail.com>
2025-02-18 22:50:00 +0200

    exercise tracking commit

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2025, 3, 1), 30, 3.0),
+            new Cycling(new DateTime(2025, 4, 1), 30, 20.0),
+            new Swimming(new DateTime(2025, 5, 1), 30, 20)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
